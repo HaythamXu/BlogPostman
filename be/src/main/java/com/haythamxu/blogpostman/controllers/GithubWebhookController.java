6a69ca3bc2f9d1132cf85330f541dcad94fd6b63
Haytham Xu <haytham.xu@sap.com>
2021-01-13 23:41:00 +0800

    callwebhook service in controller

--- be/src/main/java/com/haythamxu/blogpostman/controllers/GithubWebhookController.java
+++ be/src/main/java/com/haythamxu/blogpostman/controllers/GithubWebhookController.java
@@ -1,19 +1,23 @@
 package com.haythamxu.blogpostman.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.haythamxu.blogpostman.services.GithubService;
+
 @RestController
 public class GithubWebhookController {
 
+    @Autowired
+    private GithubService githubService;
+
     @GetMapping("/githubwebhook")
     public ResponseEntity<Void> getApplicationConfigurationDetails() {
-
+        this.githubService.pullFromGithub();
         return new ResponseEntity<>(HttpStatus.OK);
     }
-
-
 
 }
