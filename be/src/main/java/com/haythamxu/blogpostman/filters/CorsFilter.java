f21cdd503453188acddfd2997edcc33b289773af
Haytham Xu <haytham.xu@sap.com>
2021-01-18 00:34:37 +0800

    add CORS config

---
+++ be/src/main/java/com/haythamxu/blogpostman/filters/CorsFilter.java
@@ -0,0 +1,34 @@
+package com.haythamxu.blogpostman.filters;
+
+import java.io.IOException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class CorsFilter implements Filter {
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain)
+        throws IOException, ServletException {
+
+        HttpServletResponse response = (HttpServletResponse) servletResponse;
+        response.setHeader("Access-Control-Allow-Origin", "*");
+        response.setHeader("Access-Control-Allow-Credentials", "true");
+        response.setHeader("Access-Control-Allow-Methods", "*");
+        response.setHeader("Access-Control-Max-Age", "3600");
+        response.setHeader("Access-Control-Allow-Headers", "*");
+        response.setHeader("Access-Control-Request-Headers", "Authorization,Origin, X-Requested-With,content-Type,Accept");
+        response.setHeader("Access-Control-Expose-Headers", "*");
+
+        filterChain.doFilter(servletRequest, response);
+
+    }
+    
+}
